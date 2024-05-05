daf3e851aa6955c7ce8f63854f21b4af892c609f
mahima73 <59014065+mahima73@users.noreply.github.com>
2024-05-05 12:30:48 +0530

    Delete UserService directory

--- UserService/UserService/src/main/java/com/flightbookingSystem/userService/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flightbookingSystem.userService.repositories;
-
-import com.flightbookingSystem.userService.dao.UserDto;
-import com.flightbookingSystem.userService.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserName(String username);
-}
